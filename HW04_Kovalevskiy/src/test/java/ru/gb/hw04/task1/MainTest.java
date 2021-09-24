239abfb34a8766548f1c2b922eebbeae99a92c41
e.kovalevskiy <carsar.sd@gmail.com>
2021-09-25 01:50:55 +0300

    Level 3. Tuning project

---
+++ HW04_Kovalevskiy/src/test/java/ru/gb/hw04/task1/MainTest.java
@@ -0,0 +1,17 @@
+package ru.gb.hw04.task1;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import ru.gb.hw04.WatchmanExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+@ExtendWith(WatchmanExtension.class)
+public class MainTest {
+    
+
+}
