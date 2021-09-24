2dee7357c2f73a38fade4a1add0fb674efe23721
e.kovalevskiy <carsar.sd@gmail.com>
2021-09-24 19:16:04 +0300

    Level 3. Start project

---
+++ HW02_Kovalevskiy/src/test/java/ru/gb/homeworks/hw02/task1/MainTest.java
@@ -0,0 +1,17 @@
+package ru.gb.homeworks.hw02.task1;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import ru.gb.homeworks.hw02.WatchmanExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+@ExtendWith(WatchmanExtension.class)
+public class MainTest {
+    
+
+}
