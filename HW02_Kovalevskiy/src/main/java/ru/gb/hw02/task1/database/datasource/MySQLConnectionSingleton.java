82226a3dd4974cb1e21811aa3b5f2f2d21f98336
e.kovalevskiy <carsar.sd@gmail.com>
2021-10-06 04:12:44 +0300

    refs #7 - Homework #2 v.3

--- HW02_Kovalevskiy/src/main/java/ru/gb/hw02/task1/database/datasource/MySQLConnectionSingleton.java
+++ HW02_Kovalevskiy/src/main/java/ru/gb/hw02/task1/database/datasource/MySQLConnectionSingleton.java
@@ -14,8 +14,8 @@ public class MySQLConnectionSingleton {
     
     }
     
-    public static Connection getInstance() {
-        if (instance == null) {
+    public static Connection getInstance() throws SQLException {
+        if (instance == null || instance.isClosed()) {
             try {
                 instance = new DataSourceFactory().getMySQLDataSource().getConnection();
                 log.info("The new MySQLDataSource connection instance is created.");
